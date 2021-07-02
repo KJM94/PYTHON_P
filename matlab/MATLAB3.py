20a9ced54b4efae1b8042701b456500e23fffa6a
Jung-Min <59479116+KJM94@users.noreply.github.com>
2021-07-02 14:50:36 +0900

    Add files via upload

---
+++ matlab/MATLAB3.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+img_src = cv2.imread('figure/fruits_gray.png',cv2.IMREAD_COLOR)
+height, width = img_src.shape[:2]
+
+img_zero = np.zeros((height, width, 3), dtype=np.uint8)
+
+img_dst = cv2.resize(img_src, (int(width*0.5), int(height*0.5)))
+img_zero[0:int(height/2), 0:int(width/2),:] = img_dst
+
+M  = np.array([[1,0,int(width/4)],[0,1,int(height/4)]], dtype=float)
+img_dst = cv2.warpAffine(img_zero, M, (width, height))
+
+#img_result = cv2.hconcat([img_src, img_zero])
+cv2.imshow("dst", img_dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
